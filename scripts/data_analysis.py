170521ec20a9ac72e6ec81fccbe6e3fee5754286
yagyagoel1 <yagyagoel87@gmail.com>
2024-08-03 13:56:23 +0530

    commented and added some analysis parameter

--- scripts/data_analysis.py
+++ scripts/data_analysis.py
@@ -8,24 +8,26 @@ import pandas as pd
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../utils')))
 from logger import logger
 
+
 def analyze_data(df):
     
-    # Calculate total credits issued by project type and country
+    #  grouping the sum of credits by country 
     credits_by_country = df.groupby('Country')['Credits Issued'].sum().reset_index()
-    credits_by_project_type = df.groupby('Project Type')['Credits Issued'].sum().reset_index()  # Summing credits issued by project type
+    # grouping the sum of credits by project type 
+    credits_by_project_type = df.groupby('Project Type')['Credits Issued'].sum().reset_index()  
 
-    # Identifying trends in credit issuance over time
+    # grouping the sum of credits by year
     credits_by_year = df.groupby('Vintage')['Credits Issued'].sum().reset_index()
 
     
-    # Removing the extreme highest value
+    #removing the extreme highest value from the data
     max_threshold = df['Credits Issued'].quantile(0.9999)
     filtered_df = df[df['Credits Issued'] <= max_threshold]
 
-    # Ensuring the lowest value is greater than zero
+    #removing all the zero values
     filtered_df = filtered_df[filtered_df['Credits Issued'] > 0]
     
-    # Finding projects with the highest and lowest credit issuance in the filtered dataset
+    #taking the maximum and minimum credits issued of new data
     highest_credit_project = filtered_df.loc[filtered_df['Credits Issued'].idxmax()]
     lowest_credit_project = filtered_df.loc[filtered_df['Credits Issued'].idxmin()]
 
